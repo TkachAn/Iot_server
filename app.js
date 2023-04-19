ebd8b6f98f1e2a864e9caa85a11df63d1ca9baa5
TkachAn <4ndrey.tkachenko@gmail.com>
2023-04-19 13:16:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ const app = express();
 
 console.log("process.env.SECRET_KEY: ", process.env.SECRET_KEY);
 let cnt = 0;
-let cnt404 = 0;
-let cnt500 = 0;
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -38,8 +36,6 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 //
-
-
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/api/tasks", apiRouter);
@@ -48,7 +44,6 @@ app.use("/api/devices", devicesApiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  // console.log("cnt404: ", cnt404++);
   next(createError(404));
 });
 
@@ -61,7 +56,6 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
-  // console.log("cnt500: ", cnt500++);
 });
 
 module.exports = app;
