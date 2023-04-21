91ef2fdf2ee707bb8be53b6f1bf9978fc858028c
TkachAn <4ndrey.tkachenko@gmail.com>
2023-04-21 19:10:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const moment = require("moment");
 const fs = require("fs/promises");
-
+// const { connectToDb, getDb } = require("./db");
 const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
@@ -15,8 +15,11 @@ const usersApiRouter = require("./routes/api/users");
 const devicesApiRouter = require("./routes/api/devices");
 const app = express();
 
-console.log("process.env.SECRET_KEY: ", process.env.SECRET_KEY);
+// console.log("process.env.SECRET_KEY: ", process.env.SECRET_KEY);
 let cnt = 0;
+// let db;
+
+// 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -42,10 +45,6 @@ app.use("/api/tasks", apiRouter);
 app.use("/api/users", usersApiRouter);
 app.use("/api/devices", devicesApiRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -59,3 +58,44 @@ app.use(function (err, req, res, next) {
 });
 
 module.exports = app;
+
+// app.get("/IoT", (req, res) => {
+//   const users = [];
+//   db.collection("owners")
+//     .find()
+//     .forEach((i) => {
+//       // console.log("i: ", i.users);
+//       users.push(i);
+//     })
+//     .then(() => {
+//       res.status(200).json(users);
+//     })
+//     .catch(() => {
+//       res.status(500).json({error: "500"});
+//     });
+// });
+// app.post("/IoT", (req, res) => {
+//   const users = [];
+//   db.collection("owners")
+//     .insertOne(req.body)
+//     .then((result) => {
+//       res.status(200).json(result);
+//     })
+
+//     .catch(() => {
+//       res.status(500).json({error: "500"});
+//     });
+// });
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+// connectToDb((err) => {
+// 	if (!err) {
+// 		console.log("ok: ");
+// 		db = getDb();
+// 	}
+// 	else{console.log("DB connection ERROR: ", err);}
+// 	;
+	
+// });
